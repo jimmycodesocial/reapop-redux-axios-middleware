edbf0add80c3db5614142734a9b077ed60c80b5a
Alberto Menendez Romero <albertomr86@gmail.com>
2017-10-21 19:52:19 -0300

    Project config.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright(c) 2016 JimmyCode Social <hi@jimmycode.com> (https://jimmycode.com)
+ */
+
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'ReapopReduxAxiosMiddleware',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
